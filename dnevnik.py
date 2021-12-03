1d7d114ac151a3485d1c1452d15a1faff7db9913
Aleksander Tarelkin <newtover@gmail.com>
2021-12-03 21:39:15 +0300

    refactor dvevnik to work with new Dnevnik2 class

--- dnevnik.py
+++ dnevnik.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
-from collections import defaultdict
 import datetime as dt
 import json
-from pathlib import Path
-import pickle
-import pip._vendor.requests as requests
-import pip._vendor.toml as toml
 import sys
+from collections import defaultdict
+from pathlib import Path
+from typing import Dict
+
+import pip._vendor.toml as toml
+
+from dnevnik2 import Dnevnik2
 
 
-SUBJECTS = {
-    113703: "Английский язык",
-    113704: "Немецкий язык",
-}
-
-
-def get_subject(item):
-    return SUBJECTS.get(item['subject_id'], item['subject_name'])
+def get_subject(item, subjects: Dict[str, str]) -> str:
+    subject_id = str(item['subject_id'])
+    return subjects.get(subject_id, item['subject_name'])
 
 
 def to_date(text):
@@ -24,23 +21,17 @@ def to_date(text):
 
 
 def main():
-    config_path = 'app_config.toml'
-    session_path = 'dnevnik.session.pkl'
+    config_path = Path('app_config.toml')
+    cookies_path = Path('cookies.json')
     base_dir = Path(sys.argv[0]).parent.absolute()
 
     with (base_dir / config_path).open('r', encoding='utf-8') as f1:
         config = toml.load(f1)
 
-    url = config['urls']['marks_for_period']['url']
-    params = config['urls']['marks_for_period']['params']
+    dnevnik = Dnevnik2.make_from_cookies_file(base_dir / cookies_path)
+    params = config['methods']['fetch_marks_for_period']
 
-    with (base_dir / session_path).open('rb') as f1:
-        session = pickle.load(f1)
-
-    with session.get(url, params=params) as res:
-        res.raise_for_status()
-        text = res.text
-        data = res.json()
+    data = dnevnik.fetch_marks_for_period(**params)
 
     with (base_dir / 'last_res.txt').open('w', encoding='utf-8') as f1:
         print(json.dumps(data, ensure_ascii=False, indent=2), file=f1)
@@ -48,14 +39,15 @@ def main():
     out_lines = []
     grouped = defaultdict(list)
     for item in sorted(data['data']['items'], key=lambda x: (to_date(x['date']), x['estimate_value_name'])):
-        s_name = item['subject_name'] = get_subject(item)
+        s_name = item['subject_name'] = get_subject(item, config['subjects'])
         mark = item['estimate_value_name']
         if mark.isdigit():
             grouped[s_name].append(int(mark))
         comment = ('# ' + item['estimate_comment']) if item['estimate_comment'] else ''
         out_lines.append((
             to_date(item['date']),
-            "{subject_name:25s} {estimate_value_code:5s} {estimate_value_name:9s} {estimate_type_name:20s}".format(**item),
+            "{subject_name:25s} {estimate_value_code:5s} {estimate_value_name:9s} {estimate_type_name:20s}".format(
+                **item),
             comment
         ))
 
@@ -71,9 +63,6 @@ def main():
             avg = sum(grouped[s_name]) / len(grouped[s_name])
             s_marks = ' '.join(str(mark) for mark in grouped[s_name])
             print(f'{s_name:25s} : {avg:0.3f}    {s_marks}', file=f1)
-
-    with (base_dir / session_path).open('wb') as f1:
-        pickle.dump(session, f1)
 
 
 if __name__ == '__main__':
